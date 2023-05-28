fb815cab6f1fac6738c1b70cc5d9eb71c023ec33
HOJUN <bmp.tom@yahoo.com>
2023-05-28 16:21:31 +0900

    [add] dao structure

--- services/user_service.py
+++ services/user_service.py
@@ -1,20 +1,14 @@
-from motor.motor_asyncio import AsyncIOMotorClient
-from models.user import UserInDB, UserIn
+from models.user import UserIn, UserInDB
 from utils.hashing import Hasher
-from utils.config import get_settings
-
-settings = get_settings()
+from services.dao.user_dao import UserDao
 
 
 class UserService:
-    def __init__(self, db: AsyncIOMotorClient = None):
-        self.db = db or AsyncIOMotorClient(settings.mongo_db_url)
-        self.collection = self.db.get_database(
-            settings.mongo_db_name).get_collection("users")
+    def __init__(self, user_dao: UserDao):
+        self.user_dao = user_dao
 
     async def create_user(self, user: UserIn):
         user_in_db = UserInDB(
             **user.dict(), hashed_password=Hasher.get_hashed_password(user.password)
         )
-        result = await self.collection.insert_one(user_in_db.dict())
-        return str(result.inserted_id)
+        return await self.user_dao.create_user_in_db(user_in_db)
