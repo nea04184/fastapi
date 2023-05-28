d7efa4161ca45a02bb120112f483d293a638a441
HOJUN <bmp.tom@yahoo.com>
2023-05-28 10:44:35 +0900

    [fix] remove pycached

--- API/routes/user_routes.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter
-from API.controllers import user_controller
-
-user_router = APIRouter()
-
-user_router.include_router(user_controller.router)
--- main.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-from API.routes.user_routes import user_router
-
-app = FastAPI()
-
-app.include_router(user_router, prefix="/users", tags=["users"])
